0f49953e8fd3c56a24740fc289171414f988cf0b
shadish <shadtisch@gmail.com>
2024-08-10 13:52:05 -0700

    Create initial service docker composition.

---
+++ README.md
@@ -0,0 +1,1 @@
+I have regular use of a small setup like this for a number of small applications, so this is just a place for me to save off the boilerplate to get started with.
---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Configuration for PostgreSQL connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db:5432/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Model definition
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def to_dict(self):
+        return {"id": self.id, "name": self.name}
+
+# Initialize the database when the app context is created
+with app.app_context():
+    db.create_all()
+
+# CRUD Routes
+@app.route('/items', methods=['POST'])
+def create_item():
+    data = request.get_json()
+    new_item = Item(name=data['name'])
+    db.session.add(new_item)
+    db.session.commit()
+    return jsonify(new_item.to_dict()), 201
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    items = Item.query.all()
+    return jsonify([item.to_dict() for item in items])
+
+@app.route('/items/<int:item_id>', methods=['GET'])
+def get_item(item_id):
+    item = Item.query.get_or_404(item_id)
+    return jsonify(item.to_dict())
+
+@app.route('/items/<int:item_id>', methods=['PUT'])
+def update_item(item_id):
+    data = request.get_json()
+    item = Item.query.get_or_404(item_id)
+    item.name = data['name']
+    db.session.commit()
+    return jsonify(item.to_dict())
+
+@app.route('/items/<int:item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    item = Item.query.get_or_404(item_id)
+    db.session.delete(item)
+    db.session.commit()
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
